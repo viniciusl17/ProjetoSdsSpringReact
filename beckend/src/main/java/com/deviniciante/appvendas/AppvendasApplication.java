4c723f836a50cf9ad8febfd16c505777beb195f6
Vinicius Leal <viniciusleal.lc@gmail.com>
2021-09-09 20:32:14 -0300

    Projeto Criado

---
+++ beckend/src/main/java/com/deviniciante/appvendas/AppvendasApplication.java
@@ -0,0 +1,13 @@
+package com.deviniciante.appvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppvendasApplication.class, args);
+	}
+
+}
---
+++ beckend/src/test/java/com/deviniciante/appvendas/AppvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deviniciante.appvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
